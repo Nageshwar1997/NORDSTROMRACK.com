47615d2d9f8a816169f9d90894ee58365c893b84
Nageshwar <nageshpawarpatil@gmail.com>
2024-05-20 01:42:47 +0530

    Project Started & Tailwind css installed

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NORDSTRONM RACK APP</h1>
+    </div>
+  );
+}
+
+export default App;
