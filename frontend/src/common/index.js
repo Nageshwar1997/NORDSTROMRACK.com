d2cfee9bd15e7aab01d7476ef753e0c83f61948b
Nageshwar <nageshpawarpatil@gmail.com>
2024-06-17 00:38:03 +0530

    frontend Config

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,6 @@
-const backendDomain = "http://localhost:8080";
+import process from "../env";
+
+const backendDomain = process.env.REACT_APP_BACKEND_URL; // "http://localhost:8080";
 
 const SummaryApi = {
   signUp: {
