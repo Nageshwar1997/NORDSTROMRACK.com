e228fb7754066c7f8498b216e15247280869fac4
Nageshwar <nageshpawarpatil@gmail.com>
2024-06-17 00:54:33 +0530

    All Done

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -3,7 +3,8 @@ const process = {
     REACT_APP_STRIPE_PUBLIC_KEY:
       "pk_test_51PQwj2LyW1KBitgiStrbCB6eHe7foh5F5G6tx0kcPZUbAJ1ySSxb7HmnKK3uqHDOzPmBBxMwlWhOjTqrL0L1Edrv00D2HlhEt0",
     REACT_APP_CLOUD_NAME_CLOUDINARY: "drbhw0nwt",
-    REACT_APP_BACKEND_URL: "http://localhost:8080",
+    REACT_APP_BACKEND_URL:
+      "https://nordstromrack-com-git-main-nageshwar-pawars-projects.vercel.app",
   },
 };
 
